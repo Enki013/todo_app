e0322e4181143dd3e5a8f47ac5d41f1328c4565d
Enki <39771190+Enki013@users.noreply.github.com>
2023-12-30 15:43:23 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,56 +4,49 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:mockito/mockito.dart';
 import 'package:todo_app/main.dart';
 
-// SharedPreferences'ın sahte sürümünü oluşturmak için bir Mock sınıfı
 class MockSharedPreferences extends Mock implements SharedPreferences {}
 
-// SharedPreferences'ın sahte sürümü
 class FakeSharedPreferences {
   late MockSharedPreferences sharedPreferences;
 
   FakeSharedPreferences() {
     sharedPreferences = MockSharedPreferences();
-    SharedPreferences.setMockInitialValues({});
-    SharedPreferences.setMockInstance(sharedPreferences);
+    // SharedPreferences.setMockInitialValues({}); // Burada kullanılmasına gerek yok
+    // SharedPreferences.setMockInstance(sharedPreferences); // Burada kullanılmasına gerek yok
+    when(sharedPreferences.setStringList(any, any)).thenAnswer((_) => Future.value(true));
+    when(sharedPreferences.getStringList(any)).thenReturn([]);
+    when(sharedPreferences.setBool(any, any)).thenAnswer((_) => Future.value(true));
+    when(sharedPreferences.getBool(any)).thenReturn(false);
   }
 }
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Sahte SharedPreferences'ı oluştur
     final fakeSharedPreferences = FakeSharedPreferences();
 
-    // Uygulamanın ana widget'ını başlat
     await tester.pumpWidget(MyApp());
 
-    // Butonun var olduğunu doğrulayın
     expect(find.byType(ElevatedButton), findsOneWidget);
 
-    // Butona tıklayarak işlevselliği test edin
     await tester.tap(find.byType(ElevatedButton));
     await tester.pump();
 
-    // Counter'ın artıp artmadığını kontrol edin
     expect(find.text('1'), findsOneWidget);
   });
 
   testWidgets('Adding todo item test', (WidgetTester tester) async {
     final fakeSharedPreferences = FakeSharedPreferences();
-    
+
     await tester.pumpWidget(MyApp());
 
-    // Ekleme butonunun var olduğunu doğrula
     expect(find.byType(ElevatedButton), findsOneWidget);
 
-    // Yeni bir görev eklemek için TextField'a değer gir
     await tester.enterText(find.byType(TextField), 'Test Todo');
     await tester.pump();
 
-    // Ekleme butonuna tıkla
     await tester.tap(find.byType(ElevatedButton));
     await tester.pump();
 
-    // Yeni görevin listeye eklendiğini doğrula
     expect(find.text('Test Todo'), findsOneWidget);
   });
 }
